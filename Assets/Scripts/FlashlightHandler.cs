0b822665e4211113c4cfc962dbc73d89278cd11a
Nick <nickvanzo437@gmail.com>
2023-01-14 10:37:02 +0100

    Added flashligth gameobject and script

---
+++ Assets/Scripts/FlashlightHandler.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlashlightHandler : MonoBehaviour
+{
+    [SerializeField] private Light spotlight;
+    [SerializeField] private GameObject mainCamera;
+    private bool isTorchActive = false;
+
+    void Update()
+    {
+        if (Keyboard.current.tKey.wasPressedThisFrame && !isTorchActive)
+        {   
+            spotlight.gameObject.SetActive(true);
+            isTorchActive = true;
+        } else if(Keyboard.current.tKey.wasPressedThisFrame && isTorchActive) {
+            spotlight.gameObject.SetActive(false);
+            isTorchActive = false;
+        }
+        spotlight.transform.rotation = mainCamera.transform.rotation;
+    }
+}
